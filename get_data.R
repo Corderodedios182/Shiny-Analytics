de45d731215afe0f1874d47e19d2793d845425ba
Carlos Flores - crf005r <Carlos.R.Flores@walmart.com>
2021-06-10 19:01:19 -0500

    Creación funciones para leer cualquier Archivo

---
+++ get_data.R
@@ -0,0 +1,37 @@
+setwd("3_GitHub/Shiny-Analytics/")
+
+library(tidyverse)
+library(ggplot2)
+library(lubridate)
+library(car)
+library(plotly)
+library(stringr)
+
+#- Funciones de Apoyo
+
+get_n <- function(x, n_init = 1, n_end = 4) {
+  #Extraccion de n digitos en una cadena de texto
+  x %>%
+    as.character() %>%
+    substr(n_init, n_end)
+}
+
+procces_info <- function(data){
+  #Procesa Insumos para algoritmo  
+  data <- read_csv("Datos/Dataset Eventos 2020 - Sheet1.csv")
+  
+  colnames(data) <- colnames(data) %>%
+    str_replace_all("ga.","")
+  
+  data <- data %>%
+    mutate(año = get_n(data$date,1,4),
+           mes = get_n(data$date,5,6),
+           dia = get_n(data$date,7,8),
+           fecha = ymd(as.Date(paste0(año, "/",mes,"/", dia))))
+}
+
+#- Información
+
+data <- "Datos/Dataset Eventos 2020 - Sheet1.csv"
+
+data <- procces_info(data)
